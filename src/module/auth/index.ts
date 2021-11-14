71ac4e3122636ebc0d197bbbbbf2a532102f004d
Nguyen Kiet <nguyenkiet0807@gmail.com>
2021-11-14 18:13:20 +0700

    firebase login

---
+++ src/module/auth/index.ts
@@ -0,0 +1,13 @@
+import firebase from 'firebase/compat/app';
+import { auth } from '../../config/firebase';
+
+export const SignInWithSocialMedia = (provider: firebase.auth.AuthProvider) =>
+  new Promise<firebase.auth.UserCredential>((resolve, reject) => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+        resolve(result);
+      })
+      .catch((error) => reject(error));
+  });
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,15 +1,33 @@
-import React, { ReactElement, useRef, useEffect } from 'react';
+import React, { ReactElement, useRef, useEffect, useState } from 'react';
 import PageHeader from '../../components/page-header/PageHeader';
 import { FcGoogle } from 'react-icons/fc';
 import { BsFacebook } from 'react-icons/bs';
+import { Link, useHistory } from 'react-router-dom';
+import firebase from 'firebase/compat/app';
+import { SignInWithSocialMedia } from '../../module/auth';
+import { Providers } from '../../config/firebase';
 import './login.scss';
-import { firebase } from '../../firebase/firebase';
 
 interface Props {}
 
 export default function LoginPage({}: Props): ReactElement {
-  const SignInWithGoogle = () => {
-    // const google__provider = new Firebase.
+  const [authenticating, setAuthenticating] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
+  const history = useHistory();
+
+  const signInWithSocialMedia = (provider: firebase.auth.AuthProvider) => {
+    if (error !== '') setError('');
+
+    setAuthenticating(true);
+
+    SignInWithSocialMedia(provider)
+      .then((result) => {
+        history.push('/');
+      })
+      .catch((error) => {
+        setAuthenticating(false);
+        setError(error.message);
+      });
   };
 
   const mainref = useRef<HTMLHeadingElement>(null);
@@ -25,7 +43,10 @@ export default function LoginPage({}: Props): ReactElement {
         <div className="login__background"> </div>
         <div className="login__main">
           <div className="text-3xl font-semibold mb-8">Sign In</div>
-          <div className="w-full border bg-white p-4  flex justify-center items-center text-black text-lg mb-4 hover:bg-opacity-50 hover:bg-black hover:text-white transition duration-300 cursor-pointer">
+          <div
+            className="w-full border bg-white p-4  flex justify-center items-center text-black text-lg mb-4 hover:bg-opacity-50 hover:bg-black hover:text-white transition duration-300 cursor-pointer"
+            onClick={() => signInWithSocialMedia(Providers.google)}
+          >
             {' '}
             <span>Sign In With Google</span>{' '}
             <span className="text-2xl ml-2">
@@ -33,7 +54,10 @@ export default function LoginPage({}: Props): ReactElement {
             </span>
           </div>
 
-          <div className="w-full border flex items-center justify-center bg-white text-black p-4  text-lg mb-4 hover:bg-opacity-50 hover:bg-black hover:text-white transition duration-300 cursor-pointer">
+          <div
+            className="w-full border flex items-center justify-center bg-white text-black p-4  text-lg mb-4 hover:bg-opacity-50 hover:bg-black hover:text-white transition duration-300 cursor-pointer"
+            onClick={() => signInWithSocialMedia(Providers.facebook)}
+          >
             {' '}
             <span>Sign In With Facebook</span>{' '}
             <span className="text-2xl ml-2 text-blue-600">
