ea537cf37c0aeeae31242c161abcd67ef340e4fa
Nguyen Kiet <nguyenkiet0807@gmail.com>
2021-11-15 00:30:31 +0700

    scroll behavior

--- src/components/plan/PlanCard.tsx
+++ src/components/plan/PlanCard.tsx
@@ -11,14 +11,14 @@ interface Props {
 
 const PlanCard = (props: Props) => {
   return (
-    <div className="w-80 card__container p-4 mb-10">
+    <div className="w-80 card__container p-6 mb-10 drop-shadow-md">
       <div className="z-40">
         <div className="flex justify-between items-end mb-4">
           <div className="text-2xl">Standard</div>
           <div></div>
           <div>100$</div>
         </div>
-        <div>
+        <div className="h-72">
           <div className="flex justify-between text-sm mb-1 pb-1">
             <div>No limited limitedlimited limited limited</div>
             <div className="text-2xl text-green-400">
@@ -42,7 +42,7 @@ const PlanCard = (props: Props) => {
           </div>
         </div>
         <div className="flex justify-center items-center">
-          <div className="border border-white px-8 py-2 rounded-full hover:bg-primary hover:border-opacity-0 hover:scale-105 transform transition duration-300">
+          <div className="border cursor-pointer border-white px-8 py-2 rounded-full hover:bg-primary hover:border-opacity-0 hover:scale-105 transform transition duration-300">
             Subscribe
           </div>
         </div>
--- src/pages/plan/Plan.tsx
+++ src/pages/plan/Plan.tsx
@@ -1,18 +1,28 @@
 import PlanCard from 'components/plan/PlanCard';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useRef } from 'react';
 import PageHeader from '../../components/page-header/PageHeader';
 
 interface Props {}
 
 export default function Plan({}: Props): ReactElement {
+  useEffect(() => {
+    window.scrollTo({ top: 80, behavior: 'smooth' });
+  }, []);
+
   return (
     <>
       <PageHeader></PageHeader>
 
-      <div className="flex flex-col justify-center items-center ">
-        <div className="text-3xl">Choose your appropriate plan for you</div>
-        <div>
-          <PlanCard />
+      <div className="py-6">
+        <div className="flex flex-col justify-center items-center ">
+          <div className="text-3xl mb-10">
+            Choose your appropriate plan for you
+          </div>
+          <div className="md:grid grid-cols-3 gap-10">
+            <PlanCard />
+            <PlanCard />
+            <PlanCard />
+          </div>
         </div>
       </div>
     </>
