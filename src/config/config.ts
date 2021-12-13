838c46945ff10989d1511ba0a995f7f809dc810a
phongvanngo <19520834@gm.uit.edu.vn>
2021-12-13 13:41:09 +0700

    merege NKiet, removie env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,13 @@
 // Contains Global Configurations
 const config = {
   firebase: {
-    apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    apiKey: 'AIzaSyB822qyknDo8GDPTkejgh1YPkLRp8VehME',
+    authDomain: 'tmovie-authen.firebaseapp.com',
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    projectId: 'tmovie-authen',
+    storageBucket: 'tmovie-authen.appspot.com',
+    messagingSenderId: '815180203739',
+    appId: '1:815180203739:web:a5ca39b8755d6ea9c72efe',
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
   },
 };
